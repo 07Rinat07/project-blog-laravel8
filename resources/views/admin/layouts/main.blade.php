241dff87bf980653e74038fea64774f3f52b5be6
Rinat Sarmuldin <ura07srr@gmail.com>
2023-06-20 11:10:23 +0500

    add attributes for image upload_16_l

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -79,6 +79,8 @@
 <!-- daterangepicker -->
 <script src="{{asset('plugins/moment/moment.min.js') }}"></script>
 <script src="{{asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+
+<script src="{{asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
 <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
@@ -97,6 +99,10 @@
                 ['height', ['height']]
             ]
         });
+    });
+
+    $(function (){
+        bsCustomFileInput.init();
     });
 </script>
 </body>
